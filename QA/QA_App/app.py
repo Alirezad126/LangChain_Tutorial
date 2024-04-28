185d274379217b98cb2e18d0d11aefcec7044129
alirezad126-at-957583705357 <alireza.daneshvar1377@gmail.com>
2024-04-28 12:59:37 -0400

    ChatModel

---
+++ QA/QA_App/app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+from langchain_openai import OpenAI
+
+#Function to return the response
+def load_answers(question):
+    llm = OpenAI(model_name= "gpt-3.5-turbo-instruct")
+    answer=llm(question)
+    return answer
+
+
+#APP UI starts here
+st.set_page_config(page_title="LangChain Demo", page_icon=":robot:")
+st.header("LangChain Demo")
+
+
+#Gets the user input 
+def get_text():
+    input_text = st.text_input("You: ", key="input")
+    return input_text
+
+
+user_input = get_text()
+response = load_answers(user_input)
+
+
+submit = st.button("Generate")
+
+#Running the model if the submit button is clicked
+if submit: 
+    
+    st.subheader("Answer: ")
+    st.write(response)
+    
+    
